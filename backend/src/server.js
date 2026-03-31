c30f2e3429412d581c3e9a235eac2fea991834a0
agustinasc <agustina.santillancastro@gmail.com>
2026-03-31 20:38:33 -0300

    Intentando deployar

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,10 +28,9 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
-
 
 /* // si no entra en ninguna ruta
 app.use((req, res) => {
