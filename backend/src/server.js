6d72bcdaba7a5690ba3e5904849665f11199e389
agustinasc <agustina.santillancastro@gmail.com>
2026-03-31 00:38:48 -0300

    Tercer intento de subir a Render

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,7 +29,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 // fallback para React Router
-app.get("/*", (req, res) => {
+app.get((req, res) => {
   res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
 
